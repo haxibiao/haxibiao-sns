87f6ddba201a5e6f15d1c54ba3ffc8152621378d
唐淑娟 <tangshujuan@haxifang.cn>
2020-07-23 17:24:00 +0800

    tip打赏文件

---
+++ src/Tip.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Haxibiao\Sns;
+
+
+
+use Haxibiao\Base\Model;
+
+class Tip extends Model
+{
+    public $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
+
+    public function tipable()
+    {
+        return $this->morphTo();
+    }
+
+    //actionable target, 动态 - 打赏了 - 文章
+    public function target()
+    {
+        return $this->morphTo();
+    }
+}
