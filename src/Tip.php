161d36840a40de2d9fa7a880fe3a58476af1988f
ivan <ivan@haxibiao.com>
2021-01-19 16:53:01 +0800

    为Breeze重构基础模型

--- src/Tip.php
+++ src/Tip.php
@@ -3,14 +3,18 @@
 namespace Haxibiao\Sns;
 
 use Haxibiao\Breeze\Model;
+use Haxibiao\Breeze\User;
+use Haxibiao\Sns\Traits\TipResolvers;
 
 class Tip extends Model
 {
+    use TipResolvers;
+
     public $guarded = [];
 
     public function user()
     {
-        return $this->belongsTo(\App\User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function tipable()
