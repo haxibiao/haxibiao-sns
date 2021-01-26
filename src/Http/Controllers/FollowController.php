51db90b4a994c43e7447f498d2a9cef5b0c05761
蔡志刚 <caizhigang@haxibiao.com>
2021-01-26 16:15:59 +0800

    up...

---
+++ src/Http/Controllers/FollowController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Haxibiao\Sns\Http\Controllers;
+
+class FollowController extends Controller
+{
+    public function index()
+    {
+        return view('follow.index');
+    }
+}
---
+++ src/Http/Controllers/NotificationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Haxibiao\Sns\Http\Controllers;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+        return view('notications.index');
+    }
+}
