f1dfa49c4301cf98b6f48af5a33373598e3f0b55
蔡志刚 <caizhigang@haxibiao.com>
2021-01-26 16:15:22 +0800

    refactoring sns routing

--- routes/api/notification.php
+++ routes/api/notification.php
@@ -7,5 +7,3 @@ Route::middleware('auth:api')->get('notification/chats', 'NotificationController
 Route::middleware('auth:api')->get('notification/chat/{id}', 'NotificationController@chat');
 Route::middleware('auth:api')->post('notification/chat/{id}/send', 'NotificationController@sendMessage');
 Route::middleware('auth:api')->get('notifications/{type}', 'NotificationController@notifications');
-//未读消息
-Route::middleware('auth:api')->get('/unreads', 'UserController@unreads');
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 //用户的关注，粉丝，收藏，喜欢，足迹页面 web routes
+//关注
+Route::get('/follow', 'FollowController@index');
+//消息
+Route::get('/notification', 'NotificationController@index');
