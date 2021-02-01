d636e6bfc6d51680c3c9cf0bb3931477cb41f0bb
xxm <xiaoxinming@haxibiao.com>
2021-02-01 21:26:57 +0800

    fixed up:少提交一个改动的migration文件

--- database/migrations/2021_01_25_145120_rename_followed_to_followable_on_follows.php
+++ database/migrations/2021_01_25_145120_rename_followed_to_followable_on_follows.php
@@ -13,14 +13,16 @@ class RenameFollowedToFollowableOnFollows extends Migration
      */
     public function up()
     {
-        Schema::table('follows', function (Blueprint $table) {
-            if(Schema::hasColumn('follows','followed_id')){
-                $table->renameColumn('followed_id','followable_id');
-            }
-            if(Schema::hasColumn('follows','followed_type')){
-                $table->renameColumn('followed_type','followable_type');
-            }
-        });
+        if (Schema::hasTable('follows')) {
+            Schema::table('follows', function (Blueprint $table) {
+                if (Schema::hasColumn('follows', 'followed_id')) {
+                    $table->renameColumn('followed_id', 'followable_id');
+                }
+                if (Schema::hasColumn('follows', 'followed_type')) {
+                    $table->renameColumn('followed_type', 'followable_type');
+                }
+            });
+        }
     }
 
     /**
