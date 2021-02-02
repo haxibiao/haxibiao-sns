0f13fd4148128331da80ed5fa94a3f21b9e43580
蔡志刚 <caizhigang@haxibiao.com>
2021-02-02 18:28:22 +0800

    fix migration "notices"

--- database/migrations/2019_12_26_162232_create_notices_table.php
+++ database/migrations/2019_12_26_162232_create_notices_table.php
@@ -13,16 +13,22 @@ class CreateNoticesTable extends Migration
      */
     public function up()
     {
+        if(Schema::hasTable(notices)){
+            return;
+        }
         Schema::create('notices', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('user_id')->comment('用户ID');
             $table->string('title')->comment('标题');
             $table->text('content')->comment('内容');
             $table->timestamp('expires_at')->nullable()->comment('到期时间');
+            $table->unsignedInteger('to_user_id')->nullable()->comment('通知对象的用户ID');
+            $table->string('type')->default('public_notice')->comment('通知类型');
+
             $table->softDeletes();
+            $table->timestamps();
 
             $table->index('user_id');
-            $table->timestamps();
         });
     }
 
