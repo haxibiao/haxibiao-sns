12a93dd65d77c3e0d8413120ab2e6048881185f3
唐淑娟 <tangshujuan@haxifang.cn>
2021-02-19 14:48:56 +0800

    fix class name

--- database/migrations/2021_02_19_144153_add_top_to_comments.php
+++ database/migrations/2021_02_19_144153_add_top_to_comments.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTopToCommnents extends Migration
+class AddTopToComments extends Migration
 {
     /**
      * Run the migrations.
