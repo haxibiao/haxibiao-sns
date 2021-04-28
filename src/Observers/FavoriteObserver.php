636d6e57a121373fd02e271220277279d0681dd1
吴秀璋 <wuxiuzhang@haxibiao.com>
2021-04-28 15:26:30 +0800

    fix：FavoriteObserver计数失败

--- src/Observers/FavoriteObserver.php
+++ src/Observers/FavoriteObserver.php
@@ -20,10 +20,8 @@ class FavoriteObserver
     {
         //更新被喜欢对象的计数（刷新时间，更新排序）
         if ($favorable = $favorite->likable) {
-            if ($favorable->count_likes ?? null) {
-                $favorable->count_favorites = $favorable->favorites->count();
-                $favorable->save();
-            }
+            $favorable->count_favorites = $favorable->favorites->count();
+            $favorable->save();
         }
     }
 }
