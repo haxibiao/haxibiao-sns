43718bc350c5f1ec522e05f237902f7f45624224
ivan <ivan@haxibiao.com>
2021-05-18 14:07:00 +0800

    兼容某些长视频，短视频系统生成没有作者，无法奖励

--- src/Observers/CommentObserver.php
+++ src/Observers/CommentObserver.php
@@ -31,36 +31,39 @@ class CommentObserver
     {
         if ($comment->user->isBlack()) {
             $comment->status = -1;
-            $comment->save();
         }
 
-        if (blank($comment->commentable)) {
-            return;
+        //通知和奖励需要评论的对象
+        if (isset($comment->commentable)) {
+
+            //评论通知
+            event(new NewComment($comment));
+
+            //更新被评论对象的计数
+            $commentable = $comment->commentable;
+            if (Schema::hasColumn($commentable->getTable(), 'count_comments')) {
+                $commentable->count_comments = $commentable->comments()->whereNull('comment_id')->count();
+                $lou                         = $commentable->count_comments;
+            }
+
+            //兼容某些长视频，短视频系统生成没有作者，无法奖励
+            if ($author = $comment->commentable->user) {
+                $profile = $author->profile;
+                // 奖励贡献值
+                if ($comment->user->id != $author->id) {
+                    //刷新“点赞超人”任务进度
+                    Task::refreshTask($comment->user, "评论高手");
+                    $profile->increment('count_contributes', Contribute::COMMENTED_AMOUNT);
+                }
+            }
         }
 
-        //评论通知
-        event(new NewComment($comment));
-
-        //更新被评论对象的计数
-        $commentable = $comment->commentable;
-        if (Schema::hasColumn($commentable->getTable(), 'count_comments')) {
-            $commentable->count_comments = $commentable->comments()->whereNull('comment_id')->count();
-            $lou                         = $commentable->count_comments;
-        }
-
-        //更新该评论的楼数
-        $comment->lou = $lou ?? 0;
-        $commentable->save();
-
-        $profile = $comment->commentable->user->profile;
-        // 奖励贡献值
-        if ($comment->user->id != $comment->commentable->user->id) {
-            //刷新“点赞超人”任务进度
-            Task::refreshTask($comment->user, "评论高手");
-            $profile->increment('count_contributes', Contribute::COMMENTED_AMOUNT);
-        }
+        //记录
         Action::createAction('comments', $comment->id, $comment->user->id);
         Ip::createIpRecord('comments', $comment->id, $comment->user->id);
+        //更新该评论的楼数
+        $comment->lou = $lou ?? 0;
+        $commentable->saveQuietly();
     }
 
     public function deleted(comment $comment)
