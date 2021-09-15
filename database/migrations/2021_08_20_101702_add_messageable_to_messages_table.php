e3637f45c7366a084df2ad57b77c5b7e5de88ac5
蔡志刚 <caizhigang@haxibiao.com>
2021-09-15 15:58:42 +0800

    up...

--- database/migrations/2021_08_20_101702_add_messageable_to_messages_table.php
+++ database/migrations/2021_08_20_101702_add_messageable_to_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMessageableToChatsTable extends Migration
+class AddMessageableToMessagesTable extends Migration
 {
     /**
      * Run the migrations.
